196fe9e787ad63da90fb8a539372d3ce98282298
Bitkub Online <42228677+bitkub@users.noreply.github.com>
2019-07-09 15:43:52 +0700

    Create sample_balances.py

---
+++ samples/python/sample_balances.py
@@ -0,0 +1,40 @@
+import hashlib
+import hmac
+import json
+import requests
+
+# API info
+API_HOST = 'https://api.bitkub.com'
+API_KEY = 'YOUR API KEY'
+API_SECRET = b'YOUR API SECRET'
+
+def json_encode(data):
+	return json.dumps(data, separators=(',', ':'))
+
+def sign(data):
+	j = json_encode(data)
+	print('Signing payload: ' + j)
+	h = hmac.new(API_SECRET, msg=j.encode(), digestmod=hashlib.sha256)
+	return h.hexdigest()
+
+# check server time
+response = requests.get(API_HOST + '/api/servertime')
+ts = int(response.text)
+print('Server time: ' + response.text)
+
+# check balances
+header = {
+	'Accept': 'application/json',
+	'Content-Type': 'application/json',
+	'X-BTK-APIKEY': API_KEY,
+}
+data = {
+	'ts': ts,
+}
+signature = sign(data)
+data['sig'] = signature
+
+print('Payload with signature: ' + json_encode(data))
+response = requests.post(API_HOST + '/api/market/balances', headers=header, data=json_encode(data))
+
+print('Balances: ' + response.text)
